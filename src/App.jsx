b774e57765fb4d62c23a12f60a690fca3f1729d6
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-21 22:01:47 +0600

    install npm tailwind with daisy ui  and setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl text-white bg-purple-400">Vite + React</h1>
+      
+    </>
+  );
+}
+
+export default App;
