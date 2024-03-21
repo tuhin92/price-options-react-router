1e316542c8061e109299741cc78d49eb77d27aa2
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-21 22:47:49 +0600

    Responsive Navbar Using React And Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Custom_nav from "./components/Custom_nav/Custom_nav";
+// import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      {/* <Navbar></Navbar> */}
+      <Custom_nav></Custom_nav>
       <h1 className="text-7xl text-white bg-purple-400">Vite + React</h1>
-      
     </>
   );
 }
---
+++ src/components/Custom_nav/Custom_nav.jsx
@@ -0,0 +1,21 @@
+import Link from "../Link/Link";
+
+const Custom_nav = () => {
+  const routes = [
+    { id: 1, path: "/", name: "Home" },
+    { id: 2, path: "/about", name: "About" },
+    { id: 3, path: "/services", name: "Services" },
+    { id: 4, path: "/contact", name: "Contact" },
+    { id: 5, path: "/blog", name: "Blog" },
+  ];
+
+  return (
+    <nav>
+      <ul className="md:flex">
+        {routes.map(route => <Link key={route.id} route={route}></Link>)}
+      </ul>
+    </nav>
+  );
+};
+
+export default Custom_nav;
