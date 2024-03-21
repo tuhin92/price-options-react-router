0635641ca7722e312ae1683573ce5e87d2deb737
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-21 23:40:20 +0600

    Create Nav Toggle And Navbar Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import Custom_nav from "./components/Custom_nav/Custom_nav";
-// import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      {/* <Navbar></Navbar> */}
       <Custom_nav></Custom_nav>
+      <Navbar></Navbar>
       <h1 className="text-7xl text-white bg-purple-400">Vite + React</h1>
     </>
   );
--- src/components/Custom_nav/Custom_nav.jsx
+++ src/components/Custom_nav/Custom_nav.jsx
@@ -1,6 +1,12 @@
+import PropTypes from "prop-types";
 import Link from "../Link/Link";
+import { RiMenu2Line } from "react-icons/ri";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from "react";
 
 const Custom_nav = () => {
+  const [open, setOpen] = useState(false);
+
   const routes = [
     { id: 1, path: "/", name: "Home" },
     { id: 2, path: "/about", name: "About" },
@@ -11,11 +17,23 @@ const Custom_nav = () => {
 
   return (
     <nav>
+      <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
+        {
+            open === true ? 
+            <RxCross1></RxCross1>
+             : <RiMenu2Line></RiMenu2Line>
+        }
+      </div>
       <ul className="md:flex">
-        {routes.map(route => <Link key={route.id} route={route}></Link>)}
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
       </ul>
     </nav>
   );
 };
 
+Link.propTypes = {
+  route: PropTypes.object,
+};
 export default Custom_nav;
