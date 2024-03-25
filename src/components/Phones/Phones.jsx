224def16756dd07e3be46cf3cdd5a4d66f58022a
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-26 05:02:22 +0600

    Explore React Awesome Components and add loading spinner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,39 +1,66 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import { TailSpin } from "react-loader-spinner";
 
 const Phones = () => {
-    const [phones, setPhones] = useState([]);
-    useEffect(() =>{
-        // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
-        // .then(res => res.json())
-        // .then(data => setPhones(data.data))
+  const [phones, setPhones] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-        axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(data => {
-            const phoneData = data.data.data;
-            const phonesWithFakeData = phoneData.map(phone => {
-                const obj = {
-                    name: phone.phone_name,
-                    price: parseInt(phone.slug.split('-')[1])
-                }
-                return obj;
-            })
-            console.log(phonesWithFakeData);
-            setPhones(phonesWithFakeData)
+  useEffect(() => {
+    // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    // .then(res => res.json())
+    // .then(data => setPhones(data.data))
+
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const phoneData = data.data.data;
+        const phonesWithFakeData = phoneData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
         });
-    },[])
-    return (
-        <div>
-            <h2 className="text-4xl">phones: {phones.length}</h2>
-            <BarChart width={1400} height={400} data={phones}>
-                <Bar dataKey="price" fill="#8884d8" />
-                <XAxis dataKey="name"></XAxis>
-                <YAxis></YAxis>
-                <Tooltip></Tooltip>
-            </BarChart>
+        console.log(phonesWithFakeData);
+        setPhones(phonesWithFakeData);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      {loading && (
+        <div className="">
+          render(
+          <TailSpin
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+          )
         </div>
-    );
+      )}
+      <h2 className="text-4xl">phones: {phones.length}</h2>
+      <BarChart width={1400} height={400} data={phones}>
+        <Bar dataKey="price" fill="#8884d8" />
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </BarChart>
+    </div>
+  );
 };
 
 export default Phones;
