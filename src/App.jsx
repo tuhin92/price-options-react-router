1ee2ba90d8520196cd5d5a2da38df5742ce72a1c
tuhin92 <tuhinalmamun71m@gmail.com>
2024-03-25 22:12:53 +0600

    Use Reachart To Draw Any Type Of Chart Using React

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Custom_nav from "./components/Custom_nav/Custom_nav";
+import LineChart from "./components/LineChart/LineChart";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
 // import Navbar from "./components/Navbar/Navbar";
 
@@ -10,6 +11,7 @@ function App() {
       {/* <Navbar></Navbar> */}
       {/* <h1 className="text-7xl text-white bg-purple-400">Vite + React</h1> */}
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
     </>
   );
 }
---
+++ src/components/LineChart/LineChart.jsx
@@ -0,0 +1,29 @@
+import { LineChart as LC, Line, XAxis, YAxis } from "recharts";
+const LineChart = () => {
+  const studentMarksData = [
+    { id: 1, student: "Tom", math: 85, physics: 78, chemistry: 90 },
+    { id: 2, student: "Jack", math: 75, physics: 82, chemistry: 85 },
+    { id: 3, student: "Mack", math: 90, physics: 88, chemistry: 92 },
+    { id: 4, student: "Bob", math: 80, physics: 75, chemistry: 78 },
+    { id: 5, student: "Jenny", math: 65, physics: 70, chemistry: 68 },
+    { id: 6, student: "Miko", math: 70, physics: 65, chemistry: 72 },
+    { id: 7, student: "Stphin", math: 88, physics: 90, chemistry: 85 },
+    { id: 8, student: "Mario", math: 92, physics: 85, chemistry: 88 },
+    { id: 9, student: "Maria", math: 78, physics: 80, chemistry: 75 },
+    { id: 10, student: "David", math: 83, physics: 75, chemistry: 80 },
+  ];
+
+  return (
+    <div>
+      <LC width={900} height={400} data={studentMarksData}>
+        <XAxis dataKey="student" />
+        <YAxis />
+        <Line type={"monotone"} dataKey="math" stroke="#8884d8"></Line>
+        <Line type={"monotone"} dataKey="physics" stroke="red"></Line>
+        <Line type={"monotone"} dataKey="chemistry" stroke="yellow"></Line>
+      </LC>
+    </div>
+  );
+};
+
+export default LineChart;
